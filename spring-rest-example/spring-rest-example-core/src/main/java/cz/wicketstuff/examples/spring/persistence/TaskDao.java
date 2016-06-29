94c6303bb96ae490e21b589819ddbaf26fceefe4
strma17 <martin.strejc@ca.com>
2016-06-29 14:50:13 +0200

    MyBatis prepare DAO

--- spring-rest-example/spring-rest-example-core/src/main/java/cz/wicketstuff/examples/spring/persistence/TaskDao.java
+++ spring-rest-example/spring-rest-example-core/src/main/java/cz/wicketstuff/examples/spring/persistence/TaskDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
 
 import cz.wicketstuff.examples.spring.core.domain.Task;
 import cz.wicketstuff.examples.spring.core.service.Status;
@@ -13,26 +14,27 @@ import cz.wicketstuff.examples.spring.core.service.Status;
  * @author Martin Strejc (strma17)
  *
  */
+@Repository
 public interface TaskDao {
 	
 	@Select("SELECT * FROM task")
-	public List<Task> selectTasks();
+	List<Task> selectTasks();
 	
-	public Task createTask(String name);
+	Task createTask(String name);
 	
-	public Task createTask(String name, Integer priority);
+	Task createTask(String name, Integer priority);
 	
 	@Delete("DELETE FROM taks WHERE id = #{taskId}")
-	public void deleteTask(Integer taskId);
+	void deleteTask(Integer taskId);
 	
 	@Update("UPDATE task SET name = #{name} WHERE id = #{taskId}")
-	public Task changeTaskName(Integer taskId, String name);
+	Task changeTaskName(Integer taskId, String name);
 	
 	@Update("UPDATE task SET status = #{name} WHERE id = #{taskId}")
-	public Task changeTaskPriority(Integer taskId, Integer priority);
+	Task changeTaskPriority(Integer taskId, Integer priority);
 	
 	@Update("UPDATE task SET name = #{name} WHERE id = #{taskId}")
-	public Task changeTaskStatus(Integer taskId, Status status);
+	Task changeTaskStatus(Integer taskId, Status status);
 	
 
 }
---
+++ spring-rest-example/spring-rest-example-core/src/main/java/cz/wicketstuff/examples/spring/persistence/mybatis/MyBatisConfig.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.wicketstuff.examples.spring.persistence.mybatis;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import cz.wicketstuff.examples.spring.persistence.mybatis.dao.MyBatisDaoPackageMarker;
+
+/**
+ * @author Martin Strejc (strma17)
+ *
+ */
+@Configuration
+@MapperScan(basePackageClasses={MyBatisDaoPackageMarker.class})
+// @ComponentScan(basePackageClasses = {DataServicePackageMarker.class})
+public class MyBatisConfig {
+	
+	@Value("classpath:mybatis-mapperConfig.xml")
+	private Resource myBatisConfigResource;
+	
+	@javax.annotation.Resource(mappedName = "jdbc/spring_example")
+	private DataSource dataSource;
+
+	@Autowired(required = false)
+	private TransactionFactory transactionFactory;
+
+	@Bean 
+	public SqlSessionFactoryBean sqlSessionFactory() {
+		SqlSessionFactoryBean sqlSessionFactory;
+		sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource);
+		sqlSessionFactory.setTransactionFactory(transactionFactory);
+		sqlSessionFactory.setConfigLocation(myBatisConfigResource);
+		return sqlSessionFactory;
+	}
+
+}
